518c49dda283d2a0ff8e0e4010abe816eff32ea1
DonCamilo <141bkaefjan@eurogym.info>
2022-04-07 12:02:52 +0200

    1 binaries uploaded

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
     <SignupForm />
-    
   </div>
 </template>
 
 <script>
-import SignupForm from './components/SignupForm.vue'
+import SignupForm from './components/SignupForm.vue';
 
 export default {
   name: 'App',
   components: {
-    SignupForm
-  }
-}
+    SignupForm,
+  },
+};
 </script>
 
 <style>
